58716d7c3da9ee44f4eb06345b5b8994723b9144
Rafael Galani <galani.rafael@gmail.com>
2021-02-23 20:18:49 -0300

    initial commit

---
+++ test/rocketpay/numbers_test.exs
@@ -0,0 +1,23 @@
+defmodule Rocketpay.NumbersTest do
+  use ExUnit.Case
+
+  alias Rocketpay.Numbers
+
+  describe "sum_from_file/1" do
+    test "When there is a file with the given name, returns the sum of numbers" do
+      response = Numbers.sum_from_file("numbers")
+
+      expected_response = {:ok, %{result: 37}}
+
+      assert expected_response == response
+    end
+
+    test "When there is no file with the given name, returns the error" do
+      response = Numbers.sum_from_file("banana")
+
+      expected_response = {:error, %{message: "Invalid file"}}
+
+      assert expected_response == response
+    end
+  end
+end
