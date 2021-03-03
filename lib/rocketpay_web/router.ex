6ec0808a2d50d02d393d15e2e203e6355687d19a
Rafael Galani <galani.rafael@gmail.com>
2021-03-02 23:51:59 -0300

    adding deposit operation

--- lib/rocketpay_web/router.ex
+++ lib/rocketpay_web/router.ex
@@ -7,9 +7,14 @@ defmodule RocketpayWeb.Router do
 
   scope "/api", RocketpayWeb do
     pipe_through :api
+
     get "/:filename", WelcomeController, :index
 
     post "/users", UsersController, :create
+
+    post "/accounts/:id/deposit", AccountsController, :deposit
+    post "/accounts/:id/withdraw", AccountsController, :withdraw
+
   end
 
   # Enables LiveDashboard only for development
---
+++ lib/rocketpay_web/views/accounts_view.ex
@@ -0,0 +1,15 @@
+defmodule RocketpayWeb.AccountsView do
+  alias Rocketpay.Account
+  def render("update.json", %{account: %Account{
+    id: id,
+    balance: balance,
+  }}) do
+    %{
+      message: "Balance changed successfully.",
+      account: %{
+        id: id,
+        balance: balance,
+      }
+    }
+  end
+end
